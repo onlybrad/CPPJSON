1eb6d98663d78745c031570cbb09e3feaf89fe14
Singe-Horizontal <singe_horizontal@protonmail.com>
2025-01-27 22:13:11 +0100

    Visual Studio Solution

--- util.cpp
+++ util.cpp
@@ -6,7 +6,7 @@
 #include <cstdlib>
 #include <cerrno>
 #include <cstdio>
-#include <sys/time.h>
+#include <ctime>
 #include "util.hpp"
 
 namespace CJSON {
@@ -177,9 +177,9 @@ char *file_get_contents(const std::string& path, size_t &filesize) {
         return NULL;
     }
 
-    fseeko(file, 0, SEEK_END);
-    const off_t length = ftello(file);
-    fseeko(file, 0, SEEK_SET);
+    fseek(file, 0, SEEK_END);
+    const off_t length = ftell(file);
+    fseek(file, 0, SEEK_SET);
     //the buffer returned has 1 extra byte allocated in case a null terminated string is required
     char *data = new char[length + 1];
     fread(data, sizeof(char), (size_t)length, file);
@@ -187,6 +187,55 @@ char *file_get_contents(const std::string& path, size_t &filesize) {
 
     filesize = (size_t)length;
     return data;
+}
+
+#include < time.h >
+
+#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+  #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+#else
+  #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+#endif
+
+struct timezone
+{
+  int  tz_minuteswest; /* minutes W of Greenwich */
+  int  tz_dsttime;     /* type of dst correction */
+};
+
+int gettimeofday(struct timeval *tv, struct timezone *tz)
+{
+  FILETIME ft;
+  unsigned __int64 tmpres = 0;
+  static int tzflag;
+
+  if (NULL != tv)
+  {
+    GetSystemTimeAsFileTime(&ft);
+
+    tmpres |= ft.dwHighDateTime;
+    tmpres <<= 32;
+    tmpres |= ft.dwLowDateTime;
+
+    /*converting file time to unix epoch*/
+    tmpres /= 10;  /*convert into microseconds*/
+    tmpres -= DELTA_EPOCH_IN_MICROSECS;
+    tv->tv_sec = (long)(tmpres / 1000000UL);
+    tv->tv_usec = (long)(tmpres % 1000000UL);
+  }
+
+  if (NULL != tz)
+  {
+    if (!tzflag)
+    {
+      _tzset();
+      tzflag++;
+    }
+    tz->tz_minuteswest = _timezone / 60;
+    tz->tz_dsttime = _daylight;
+  }
+
+  return 0;
 }
 
 long usec_timestamp() {
