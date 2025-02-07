490f6995419e22815fa620d33c170a2b46a5e5d7
Only Brad <bradonly420@gmail.com>
2025-02-07 14:48:09 -0500

    replaced absolute path with relative path in test2.cpp

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     size_t filesize;
-    char *const data = CJSON::file_get_contents("E:\\code\\cpp\\json\\tests\\really-big-json-file.json", filesize);
+    char *const data = CJSON::file_get_contents("tests\\really-big-json-file.json", filesize);
     
     const long start = CJSON::usec_timestamp();
     auto json = CJSON::parse(data);
