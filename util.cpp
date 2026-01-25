b213c3c25b48cef8e2b87136ec7a710ee52a4e8b
Only Brad <bradonly420@gmail.com>
2026-01-25 18:59:10 -0500

    added better check for control char in Parser::decodeStringToken

--- util.cpp
+++ util.cpp
@@ -44,21 +44,11 @@ bool isDelimiter(const char c) noexcept {
 }
 
 bool isDigit(const char c) noexcept {
-    switch(c) {
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-        return true;
-    default:
-        return false;
-    }
+    return c >= '0' && c <= '9';
+}
+
+bool isControlChar(const char c) noexcept {
+    return c >= static_cast<char>(0x00) && c <= static_cast<char>(0x1F);
 }
 
 std::uint16_t hexToUtf16(const char *const codepoint, bool &success) noexcept {
